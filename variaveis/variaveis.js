940da459a1effd4af9e7d9ecd7dda2000b46c707
locideran <gabriel-masc@hotmail.com>
2023-07-03 21:28:14 -0300

    variáveis

---
+++ variaveis/variaveis.js
@@ -0,0 +1,26 @@
+// let idade = 3
+// console.log(idade)
+// let altura = 180
+// console.log(altura)
+// camelCase
+
+let nomeCompleto = 'Gabriel Mascarenhas de Andrade Vilanova'
+console.log(nomeCompleto)
+
+let urunko = 'Locideran'
+console.log(urunko)
+
+let idade = 27
+console.log(idade)
+
+let altura = 166
+console.log(altura)
+
+let cidade = 'Aracaju'
+console.log(cidade)
+
+let estado = 'Sergipe'
+console.log(estado)
+
+let pais = 'Brasil'
+console.log(pais)
