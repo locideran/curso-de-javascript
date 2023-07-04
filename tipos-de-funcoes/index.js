774bc4581cbf4b4d2560f9f36a838a1735341bbe
locideran <gabriel-masc@hotmail.com>
2023-07-04 16:27:01 -0300

    tipos de funções

---
+++ tipos-de-funcoes/index.js
@@ -0,0 +1,16 @@
+// Realizar uma tarefa, não devolve nada
+
+function dizerNome() {
+    console.log('Gabriel')
+}
+
+dizerNome()
+//Faz um cálculo ou operação e retorna algo
+function MultiplicarPorDois(valor) {
+    return valor* 2
+}
+
+//console.log(MultiplicarPorDois(5))
+
+let res = MultiplicarPorDois(5)
+console.log(res)
