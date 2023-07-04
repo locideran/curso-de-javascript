eb98f786e688aa8a1588faab8dcf45699e73f67e
locideran <gabriel-masc@hotmail.com>
2023-07-04 15:59:54 -0300

    objetos

---
+++ objetos/index.js
@@ -0,0 +1,14 @@
+/*let nome = 'Rafael'
+let idade = 25
+let estaAprovado = true
+let sobrenome = undefined*/
+
+let pessoa = {
+    nome: 'Gabriel',
+    idade: 28,
+    estaAprovado: true,
+    sobrenome: 'Mascarenhas'
+}
+
+console.log(pessoa)
+
