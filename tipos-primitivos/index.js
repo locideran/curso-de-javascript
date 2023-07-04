5226bddc3ccd1f3596f5442c76aaefe0e7f45a7e
locideran <gabriel-masc@hotmail.com>
2023-07-04 15:31:38 -0300

    tipos primitivos

---
+++ tipos-primitivos/index.js
@@ -0,0 +1,5 @@
+let nome = 'Rafael' //string literal
+let idade = 25 //number literal
+let estaAprovado = true //boolean
+let sobrenome = undefined//Undefined
+let corSelecionado = null //Redefinir um valor
