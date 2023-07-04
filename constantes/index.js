b470f9c83d5dd0586844d1dc9162f351f3b0fe92
locideran <gabriel-masc@hotmail.com>
2023-07-04 15:21:08 -0300

    constantes

---
+++ constantes/index.js
@@ -0,0 +1,9 @@
+const valorIngressoAdulto = 20
+
+console.log(valorIngressoAdulto)
+
+const idade = 17
+console.log(idade)
+
+const nomeCompleto = 'Gabriel Mascarenhas de Andrade Vilanova'
+console.log(nomeCompleto)
