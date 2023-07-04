c1341523705b5f3804160c2b2b5bf2d2466e2986
locideran <gabriel-masc@hotmail.com>
2023-07-04 16:21:34 -0300

    funções

---
+++ funcoes/index.js
@@ -0,0 +1,13 @@
+//Verbo + Substantivo
+let corSite = "Azul"
+function resetaCor(cor, tonalidade) {
+    corSite = cor + tonalidade
+} 
+
+console.log(corSite)
+resetaCor("verde"," claro")
+console.log(corSite)
+
+/*console.log(corSite)
+resetaCor()
+console.log(corSite)*/
