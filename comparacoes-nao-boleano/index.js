711a95dbca91ef8af09180a2b024454d9cf66fa2
locideran <gabriel-masc@hotmail.com>
2023-07-06 15:16:26 -0300

    comparações não boleanas

---
+++ comparacoes-nao-boleano/index.js
@@ -0,0 +1,16 @@
+/* Falsy 
+underfined
+null
+0
+''
+NaN - not a number
+*/
+
+/* Truthy
+
+*/
+
+let corPersonalizada = 'Vermelho'
+let corPadrao = 'Azul'
+let corPerfil = corPersonalizada || corPadrao
+console.log(corPerfil)
