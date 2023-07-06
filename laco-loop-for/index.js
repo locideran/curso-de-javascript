4ee523cd69ec24460c8e976b9868ca34861415c0
locideran <gabriel-masc@hotmail.com>
2023-07-06 15:55:23 -0300

    for..while

--- laco-loop-for/index.js
+++ laco-loop-for/index.js
@@ -6,8 +6,16 @@
 */
 
 // 1. For
-for(let i = 5; i >= 0; i--) {
-    if(i % 2 !==0) {
+// for(let i = 5; i >= 0; i--) {
+//     if(i % 2 !==0) {
+//         console.log(i)
+//     }
+// }
+
+let i = 5
+while (i >= 1) {
+    if (i % 2 !== 0) {
         console.log(i)
     }
+    i--
 }
