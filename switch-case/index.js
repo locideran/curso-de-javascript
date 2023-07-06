a16002554cfef43c8b98908b783ec34a3ae19526
locideran <gabriel-masc@hotmail.com>
2023-07-06 15:37:51 -0300

    switch..case

---
+++ switch-case/index.js
@@ -0,0 +1,17 @@
+let permissao //comum, gerente, diretor
+permissao = 'gerente' 
+switch (permissao) {
+    case 'comum':
+        console.log('usuário comum')
+        break;
+    case 'gerente':
+        console.log('usuário gerente')
+        break;
+    case 'diretor':
+        console.log('usuário diretor')
+        break;    
+
+    default: 
+    console.log('Usuário não reconhecido!')
+        break;
+}
