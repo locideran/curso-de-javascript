82438aba5f274db3324d5235db40c8a592030eb3
locideran <gabriel-masc@hotmail.com>
2023-07-07 18:05:56 -0300

    exercício

---
+++ maximo-entre-valores/index.js
@@ -0,0 +1,10 @@
+// Escreva uma função que usa 2 números e retorna o maior entre eles
+
+let valorMaior = max(5,9)
+console.log(valorMaior)
+
+function max(num1,num2) {
+    if(num1 > num2)
+    return num1
+    else return num2
+}
