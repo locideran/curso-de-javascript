0583c1db2cfe14640d524a156236fdb0d0b96b3f
locideran <gabriel-masc@hotmail.com>
2023-07-09 10:22:17 -0300

    exercício string

---
+++ strings/index.js
@@ -0,0 +1,14 @@
+// Criar um método para ler propriedades de um objeto e exibir somente as propriedades do tipo string que estão nesse objeto
+
+const filme = {
+    titulo: 'Vingadores',
+    ano: 2018,
+    diretor: 'Robin',
+    personagem: 'Thor'
+}
+exibirPropriedades(filme)
+function exibirPropriedades(obj) {
+    for (prop in obj)
+        if(typeof prop === 'string')
+            console.log(prop,obj[prop])
+}
