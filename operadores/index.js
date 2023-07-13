974d9ee76ff93c57402e0d1c44c3ae6f7c9a287e
locideran <gabriel-masc@hotmail.com>
2023-07-13 17:54:17 -0300

    operadores

--- operadores/index.js
+++ operadores/index.js
@@ -1,6 +1,19 @@
-    
+/* Vamos entender Operadores: 
+-> Os operadores JavaScript são usados para atribuir valores, comparar valores, executar operações aritméticas e muito mais. 
 
-//
+São os sinais que usamos: + - * / = ++ -- += -= && || etc...
+
+São separados em 6 "categorias": 
+
+1) Operadores Aritméticos (matemáticos)
+2) Operadores de Atribuição
+3) Operadores de Sequência 
+4) Operadores de Comparação
+5) Operador Condicional (Ternário)
+6) Operadores Lógicos
+*/
 
 
+
+//valor1 += valor2 //mesmo que valor1 = valor1 + valor2
 
