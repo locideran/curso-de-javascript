78dcde16ad4f36b1222d15ec2ed0f3f45f5193bf
locideran <gabriel-masc@hotmail.com>
2023-07-13 18:27:23 -0300

    começando funções

--- funcoes/index.js
+++ funcoes/index.js
@@ -1,13 +1,15 @@
-//Verbo + Substantivo
-let corSite = "Azul"
-function resetaCor(cor, tonalidade) {
-    corSite = cor + tonalidade
-} 
+/* FUNÇÕES
+Uma função JavaScript é um bloco de código projetado para executar uma tarefa específica.
 
-console.log(corSite)
-resetaCor("verde"," claro")
-console.log(corSite)
+É como uma pequena "fábrica" onde você faz uma entrada e ele te dá uma saída.
 
-/*console.log(corSite)
-resetaCor()
-console.log(corSite)*/
+Pode ser encarado como "mini-programas" projetados para fazer uma tarefa que vai contribuir para todo código.
+
+Uma função JavaScript é executada quando "algo" a invoca (chama-a).
+*/
+
+function soma(valor1,valor2) {
+    return valor1 + valor2;    
+}
+
+    document.getElementById
