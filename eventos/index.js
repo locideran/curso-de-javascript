aadbc34b5ddff12ad88a30573cdcab7f5a6333e9
locideran <gabriel-masc@hotmail.com>
2023-07-15 18:33:31 -0300

    anotação

--- eventos/index.js
+++ eventos/index.js
@@ -14,4 +14,10 @@ onmouseup -> Disparado quando o clique do botão é liberado
 onfocus -> Disparado quando o elemento recebe o foco. Válido para input,
 onchange -> Disparado quando existe uma mudança no conteudo. "Ao mudar".
 onblur -> Disparado quando o elemento perde o foco. 
+onkeydown -> Disparado quando uma tecla é pressionada
+onkeypress -> Disparado quando uma tecla é pressionada e solta
+onkeyup -> Disparado quando uma tecla é solta sobre um elemento
+onload -> Disparado quando a página terminou de ser carregada. Body.
+onresize -> Disparado quando há um redimencionamento da janela
+
 */
