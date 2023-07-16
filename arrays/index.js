2bde17306675c1cef614b1fcdf3cc1e9757d5e83
locideran <gabriel-masc@hotmail.com>
2023-07-16 10:25:49 -0300

    arrays

--- arrays/index.js
+++ arrays/index.js
@@ -1,5 +1,23 @@
-/*let familia = [26,45,50,17]
-console.log(familia.length)
-console.log(familia[3])*/
-let nomeDoColega = [27,'Gabriel','Aracaju','Brasil']
-console.log(nomeDoColega)
+/* ARRAYS
+Os arrays JavaScript são usados para amazenar vários valores em uma única variável. Diferente dos objetos, que funcionm com propriedades ou "nomes" que você dá para os itens dentro dele, os arrays não possuem propriedades. O item dentro dele é encontrado pela posição. 
+
+Imagine um array como uma lista de itens, com controle, por ordem de posição dentro dele.
+
+Ex: const lista = ["arroz", "feijão", "macarrão", "leite"];
+
+A lista[0] (lista na posição 0) vai conter o valor "arroz".
+A lista[1] (lista na posição 1) vai conter o valor "feijão".
+
+E assim por diante.
+
+*/
+
+
+const lista = [
+    "arroz",
+    "feijão",
+    "leite",
+    "macarrão"
+];
+
+alert(lista[3]);
